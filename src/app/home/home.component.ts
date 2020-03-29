f4493234f918d2bab3dc0258d0608e1c416bae71
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-03-29 11:42:22 +0800

    修复greet类型属性不存在的错误

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../servicce/auth.service';
 
+interface Greet{
+  id:string,
+  content:string
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,15 +14,15 @@ import { AuthService } from '../servicce/auth.service';
 })
 export class HomeComponent implements OnInit {
   title = 'Demo';
-  greeting = {};
+  greeting:Greet;
   
   constructor(
     private app: AuthService,
     private http: HttpClient
   ) { }
-
+ 
   ngOnInit(): void {
-    this.http.get('http://localhost:8080/resource',{
+    this.http.get<Greet>('http://localhost:8080/resource',{
       withCredentials:true 
     }).subscribe(data => this.greeting = data);
   }
